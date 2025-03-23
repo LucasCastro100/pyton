4529b0b7afce94b25c31fcf1c2cc9658f9a64ff9
LucasCastro100 <lucascastro121295@gmail.com>
2025-03-23 13:55:07 -0300

    Subindo arquivos

--- fundamentals/database/src/main.py
+++ fundamentals/database/src/main.py
@@ -1,5 +1,9 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.operations import CreateTable, AlterTable, DropTable
 
-CreateTable("CREATE TABLE exemplo (id INT PRIMARY KEY)").execute()
+CreateTable("CREATE TABLE teste2 (id INT PRIMARY KEY)").execute()
 # AlterTable("ALTER TABLE exemplo ADD COLUMN nome VARCHAR(255)").execute()
 # DropTable("DROP TABLE exemplo").execute()
